ebe6e7860332d25d424b881fbd0519c7fbb223fc
ViniciusMartinsFaria <81265270+ViniciusMartinsFaria@users.noreply.github.com>
2024-02-22 12:26:17 -0300

    Update tb_codec.vhd
    - Versão atualizada do testbench compatível com o codificador

--- tb_codec.vhd
+++ tb_codec.vhd
@@ -1,6 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_signed.all;
+use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
 
@@ -10,27 +10,27 @@ end tb_codec;
 architecture behavior of tb_codec is
 component codificador is
 port(
-    clock           : in  std_logic;
-    reset           : in  std_logic;
-    entrada         : in  std_logic;
-    dist_cumula     : out std_logic_vector(7 downto 0);
-    prob            : out std_logic_vector(7 downto 0));
+    clock                   : in  std_logic;
+    reset                   : in  std_logic;
+    entrada                 : in  std_logic;
+    distribuicao_cumulativa : out std_logic_vector(7 downto 0);
+    probabilidade           : out std_logic_vector(7 downto 0));
 end component;
 
 signal  clock_sg        : std_logic := '0';
 signal  reset_sg        : std_logic := '0';
-signal  entrada_sg      : std_logic := '0';
-signal  dist_cumula_sg  : std_logic_vector(7 downto 0) := (others => '0');
-signal  prob_sg         : std_logic_vector(7 downto 0) := ("00000001");
+signal  entrada_sg      : std_logic;
+signal  dist_cumula_sg  : std_logic_vector(7 downto 0);
+signal  prob_sg         : std_logic_vector(7 downto 0);
 
 begin
 inst_codec: codificador
 port map (
-        clock       => clock_sg,
-        reset       => reset_sg,
-        entrada     => entrada_sg,
-        dist_cumula => dist_cumula_sg,
-        prob        => prob_sg
+        clock                   => clock_sg,
+        reset                   => reset_sg,
+        entrada                 => entrada_sg,
+        distribuicao_cumulativa => dist_cumula_sg,
+        probabilidade           => prob_sg
     );
            
 clock_sg <= not clock_sg after 10 ns;
@@ -38,15 +38,9 @@ clock_sg <= not clock_sg after 10 ns;
 process
 begin
 	wait for 5 ns;
-                entrada_sg <= '1';
-	wait for  15 ns;
-		entrada_sg <= '0';
-	wait for 25 ns;
-                entrada_sg <= '1';
-	wait for 35 ns;
-                entrada_sg <= '0';
-	wait for 45 ns;
-                entrada_sg <= '1';
+        entrada_sg <= '0';
+    wait for 20 ns;
+        entrada_sg <= '1';
 	wait;
 end process;
 
