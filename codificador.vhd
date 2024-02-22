caf6893059f4e1f391ab11b1098d32a37d5f6543
ViniciusMartinsFaria <81265270+ViniciusMartinsFaria@users.noreply.github.com>
2024-02-22 12:25:24 -0300

    Update codificador.vhd
    - Lógicas aritméticas funcionais
    - Redundâncias removidas

--- codificador.vhd
+++ codificador.vhd
@@ -1,51 +1,47 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_signed.all;
+use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
 entity codificador is
     port(
-        clock           : in  std_logic;
-        reset           : in  std_logic;
-        entrada         : in  std_logic;
-        dist_cumula     : out std_logic_vector(7 downto 0);
-        prob            : out std_logic_vector(7 downto 0));
+        clock                   : in  std_logic;
+        reset                   : in  std_logic;
+        entrada                 : in  std_logic;
+        distribuicao_cumulativa : out std_logic_vector(7 downto 0) := (others => '0');
+        probabilidade           : out std_logic_vector(7 downto 0) := (others => '1'));
 end codificador;
 
 architecture behavior of codificador is
 
-    signal base_cumula  : std_logic_vector(7 downto 0);
-    signal lenght_prob  : std_logic_vector(7 downto 0);
+    signal base         : std_logic_vector(7 downto 0);
+    signal lenght       : std_logic_vector(7 downto 0);
+
     signal eq_base      : std_logic_vector(15 downto 0);
     signal eq_lenght    : std_logic_vector(15 downto 0);
-    signal ff_base      : std_logic_vector(7 downto 0);
-    signal ff_lenght    : std_logic_vector(7 downto 0);
 
 begin
 
-    base_cumula <=  "00000011" when entrada = '0' else
-                    "00000100";
-    lenght_prob <=  "00000011" when entrada = '0' else
-                    "00000100";
+    base    <=  "00000000" when entrada = '0' else
+                "01001100";
+    lenght  <=  "01001100" when entrada = '0' else
+                "10110011";
 
-    eq_base     <=  ff_base + base_cumula * ff_lenght;
-    eq_lenght   <=  lenght_prob * ff_lenght;
-
-    dist_cumula <= ff_base;
-    prob        <= ff_lenght;
+    eq_base     <=  distribuicao_cumulativa + base * probabilidade;
+    eq_lenght   <=  lenght * probabilidade;
 
     process(clock, reset)
 	begin
 
 		if (reset = '1') then
 
-            ff_base     <= (others => '0');
-            ff_lenght   <= ("00000001");
+            distribuicao_cumulativa <= (others => '0');
+            probabilidade           <= (others => '1');
 
         else if(rising_edge(clock)) then
 
-            ff_base     <= eq_base  (7 downto 0);
-            ff_lenght   <= eq_lenght(7 downto 0);
+            distribuicao_cumulativa <= eq_base  (15 downto 8);
+            probabilidade           <= eq_lenght(15 downto 8);
 
         end if;
     end if;
